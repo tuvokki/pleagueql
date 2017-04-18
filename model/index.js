e549382a008ba872e8b5e323edae50a3b9184e77
Wouter Roosendaal <tuvokki@gmail.com>
2017-04-19 00:32:52 +0200

    Initial project

---
+++ model/index.js
@@ -0,0 +1,11 @@
+const models = {};
+
+export default function addModelsToContext(context) {
+    const newContext = Object.assign({}, context);
+    Object.keys(models).forEach((key) => {
+        newContext[key] = new models[key](newContext);
+    });
+    return newContext;
+}
+
+//
