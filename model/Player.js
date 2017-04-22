6485f1687c5549a03f8aacf0fee2060104824c68
Wouter Roosendaal <tuvokki@gmail.com>
2017-04-22 14:26:15 +0200

    Some more progress

--- model/Player.js
+++ model/Player.js
@@ -2,63 +2,67 @@ import DataLoader from 'dataloader';
 import findByIds from 'mongo-find-by-ids';
 
 export default class Player {
-  constructor(context) {
-    this.context = context;
-    this.collection = context.db.collection('player');
-    this.pubsub = context.pubsub;
-    this.loader = new DataLoader(ids => findByIds(this.collection, ids));
-  }
+    constructor(context) {
+        this.context = context;
+        this.collection = context.db.collection('players');
+        this.pubsub = context.pubsub;
+        this.loader = new DataLoader(ids => findByIds(this.collection, ids));
+    }
 
-  findOneById(id) {
-    return this.loader.load(id);
-  }
+    findOneById(id) {
+        return this.loader.load(id);
+    }
 
-  all({ lastCreatedAt = 0, limit = 10 }) {
-    return this.collection.find({
-      createdAt: { $gt: lastCreatedAt },
-    }).sort({ createdAt: 1 }).limit(limit).toArray();
-  }
+    all({ lastCreatedAt = 0, limit = 10 }) {
+        return this.collection.find({}).sort({ elo: -1 }).limit(limit).toArray();
+    }
 
-  joinDate(player) {
-    return this.context.Date.findOneById(player.joinDateId);
-  }
+    active({ lastCreatedAt = 0, limit = 10 }) {
+        return this.collection.find({
+            retired: { $in: [null, false] }
+        }).sort({ elo: -1 }).limit(limit).toArray();
+    }
 
-  belongsTo(player) {
-    return this.context.User.findOneById(player.belongsToId);
-  }
+    joinDate(player) {
+        return this.context.Date.findOneById(player.joinDateId);
+    }
 
-  lastPlayed(player, args) {
-    return this.context.Date.collection.find({
-      _id: { $in: player.lastPlayedIds || [] },
-      createdAt: { $gt: lastCreatedAt },
-    }).sort({ createdAt: 1 }).limit(limit).toArray();
-  }
+    belongsTo(player) {
+        return this.context.User.findOneById(player.belongsToId);
+    }
 
-  async insert(doc) {
-    const docToInsert = Object.assign({}, doc, {
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    });
-    const id = (await this.collection.insertOne(docToInsert)).insertedId;
-    this.pubsub.publish('playerInserted', await this.findOneById(id));
-    return id;
-  }
+    lastPlayed(player, args) {
+        return this.context.Date.collection.find({
+            _id: { $in: player.lastPlayedIds || [] },
+            createdAt: { $gt: lastCreatedAt },
+        }).sort({ createdAt: 1 }).limit(limit).toArray();
+    }
 
-  async updateById(id, doc) {
-    const ret = await this.collection.update({ _id: id }, {
-      $set: Object.assign({}, doc, {
-        updatedAt: Date.now(),
-      }),
-    });
-    this.loader.clear(id);
-    this.pubsub.publish('playerUpdated', await this.findOneById(id));
-    return ret;
-  }
+    async insert(doc) {
+        const docToInsert = Object.assign({}, doc, {
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+        });
+        const id = (await this.collection.insertOne(docToInsert)).insertedId;
+        this.pubsub.publish('playerInserted', await this.findOneById(id));
+        return id;
+    }
 
-  async removeById(id) {
-    const ret = this.collection.remove({ _id: id });
-    this.loader.clear(id);
-    this.pubsub.publish('playerRemoved', id);
-    return ret;
-  }
+    async updateById(id, doc) {
+        const ret = await this.collection.update({ _id: id }, {
+            $set: Object.assign({}, doc, {
+                updatedAt: Date.now(),
+            }),
+        });
+        this.loader.clear(id);
+        this.pubsub.publish('playerUpdated', await this.findOneById(id));
+        return ret;
+    }
+
+    async removeById(id) {
+        const ret = this.collection.remove({ _id: id });
+        this.loader.clear(id);
+        this.pubsub.publish('playerRemoved', id);
+        return ret;
+    }
 }
--- resolvers/Player.js
+++ resolvers/Player.js
@@ -1,50 +1,50 @@
 const resolvers = {
-  Player: {
-    id(player) {
-      return player._id;
-    },
+    Player: {
+        id(player) {
+            return player._id;
+        },
 
-    joinDate(player, args, { Player }) {
-      return Player.joinDate(player);
-    },
+        // joinDate(player, args, { Player }) {
+        //   return Player.joinDate(player);
+        // },
 
-    belongsTo(player, args, { Player }) {
-      return Player.belongsTo(player);
-    },
+        // belongsTo(player, args, { Player }) {
+        //   return Player.belongsTo(player);
+        // },
 
-    lastPlayed(player, args, { Player }) {
-      return Player.lastPlayed(player, args);
+        // lastPlayed(player, args, { Player }) {
+        //   return Player.lastPlayed(player, args);
+        // },
     },
-  },
-  Query: {
-    players(root, { lastCreatedAt, limit }, { Player }) {
-      return Player.all({ lastCreatedAt, limit });
-    },
+    Query: {
+        players(root, { lastCreatedAt, limit }, { Player }) {
+            return Player.active({ lastCreatedAt, limit });
+        },
 
-    player(root, { id }, { Player }) {
-      return Player.findOneById(id);
+        player(root, { id }, { Player }) {
+            return Player.findOneById(id);
+        },
     },
-  },
-  Mutation: {
-    async createPlayer(root, { input }, { Player }) {
-      const id = await Player.insert(input);
-      return Player.findOneById(id);
-    },
+    Mutation: {
+        async createPlayer(root, { input }, { Player }) {
+            const id = await Player.insert(input);
+            return Player.findOneById(id);
+        },
 
-    async updatePlayer(root, { id, input }, { Player }) {
-      await Player.updateById(id, input);
-      return Player.findOneById(id);
-    },
+        async updatePlayer(root, { id, input }, { Player }) {
+            await Player.updateById(id, input);
+            return Player.findOneById(id);
+        },
 
-    removePlayer(root, { id }, { Player }) {
-      return Player.removeById(id);
+        removePlayer(root, { id }, { Player }) {
+            return Player.removeById(id);
+        },
     },
-  },
-  Subscription: {
-    playerCreated: player => player,
-    playerUpdated: player => player,
-    playerRemoved: id => id,
-  },
+    Subscription: {
+        playerCreated: player => player,
+        playerUpdated: player => player,
+        playerRemoved: id => id,
+    },
 };
 
 export default resolvers;
